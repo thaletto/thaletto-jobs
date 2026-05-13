7db891692c08aadeec5e2e93ae74d2d760ff800d
thaletto <krlaxman03@gmail.com>
2026-05-14 01:26:53 +0530

    feat: add public API exports in src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-import { Effect } from "effect";
-
-Effect.succeed(199)
+export { VectorStore } from "./adapters/zvec/vector-store.ts";
+export { VectorStoreLive } from "./adapters/zvec/vector-store-live.ts";
+export { ZVecCollection, ZVecCollectionConfig, ZVecCollectionLive } from "./adapters/zvec/collection.ts";
+export { VectorMetadata, VectorId, StoredEntry, SearchResult, SearchOptions, SerializedRow } from "./schema/index.ts";
+export { VectorStoreError, VectorNotFoundError, VectorDecodeError, type VectorStoreErrors } from "./errors/index.ts";
