e32344c14bcdc9ba115309cec4f91a40660fff34
thaletto <krlaxman03@gmail.com>
2026-05-15 01:05:20 +0530

    feat: add Fumadocs scaffolding for documentation

---
+++ docs/src/lib/layout.shared.tsx
@@ -0,0 +1,27 @@
+import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import { appName, gitConfig } from './shared';
+
+const NpmIcon = () => (
+  <svg viewBox="0 0 2500 2500" xmlns="http://www.w3.org/2000/svg" width="20" height="20">
+    <path fill="#c00" d="M0 0h2500v2500H0z" />
+    <path fill="#fff" d="M1241.5 268.5h-973v1962.9h972.9V763.5h495v1467.9h495V268.5z" />
+  </svg>
+);
+
+export function baseOptions(): BaseLayoutProps {
+  return {
+    nav: {
+      title: appName,
+    },
+    githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`,
+    links: [
+      {
+        type: 'icon',
+        label: 'npm',
+        icon: <NpmIcon />,
+        text: 'npm',
+        url: `https://www.npmjs.com/package/@${gitConfig.user}/${gitConfig.repo}`,
+      },
+    ],
+  };
+}
---
+++ docs/src/lib/shared.ts
@@ -0,0 +1,10 @@
+export const appName = 'Cortex';
+export const docsRoute = '/docs';
+export const docsImageRoute = '/og/docs';
+export const docsContentRoute = '/llms.mdx/docs';
+
+export const gitConfig = {
+  user: 'thaletto',
+  repo: 'cortex',
+  branch: 'main',
+};
