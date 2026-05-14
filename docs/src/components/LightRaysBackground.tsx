c6d42f8fd815012f50472ec3d69276db33397fee
thaletto <krlaxman03@gmail.com>
2026-05-15 02:57:26 +0530

    fix: update LightRays properties for improved visual effects

--- docs/src/components/LightRaysBackground.tsx
+++ docs/src/components/LightRaysBackground.tsx
@@ -16,17 +16,17 @@ export default function LightRaysBackground() {
     >
       <LightRays
         raysOrigin="top-center"
-        raysColor="#7dd3fc"
+        raysColor="#55A336"
         raysSpeed={0.8}
         lightSpread={0.8}
-        rayLength={2.5}
+        rayLength={3}
         pulsating={true}
-        fadeDistance={1.2}
+        fadeDistance={2}
         saturation={1.2}
         followMouse={true}
         mouseInfluence={0.15}
-        noiseAmount={0.02}
-        distortion={0.1}
+        noiseAmount={0.2}
+        distortion={0.2}
       />
     </div>
   );
